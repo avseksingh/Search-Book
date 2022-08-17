460c67e4f447e98b10f501cf9e15a46e8604a0eb
Abhishek Singh <avsekjob@gmail.com>
2022-08-17 18:54:49 +0530

    .

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import requests
 from .models import Greeting
 
 # Create your views here.
-def quiz(request):
+def booksearch(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "quiz.html")
+    searchValue = input("Enter search value\n")
+    link = "https://www.googleapis.com/books/v1/volumes?q={0}".format(searchValue)
+    print(link)
+    response = requests.get(link)
+    print(response.json())
+    books = response.json()["items"]
+    print(books)
+    for book in books:
+        print(book)
+    return render(request, "searchbook.html", {"book":book})
 
 
 def index(request):
